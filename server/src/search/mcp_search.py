95a1ae3c8e9fe0c805965eb1d7edb941616b7537
Binimal101 <matthewtujague@gmail.com>
2025-09-28 08:01:03 -0400

    .

--- server/src/search/mcp_search.py
+++ server/src/search/mcp_search.py
@@ -5,25 +5,63 @@ from src import _TZ, logger
 
 #TODO database abstractions 
 get_all_organization_departments = lambda: [None,] * 6
-get_people_from_organization = lambda org: [None] * 6
-get_all_tasks_from_organization = lambda org, start_date, end_date: [None] * 6
+get_people_from_department = lambda dept: [None] * 6
+get_all_tasks_from_department = lambda dept, start_date, end_date: [None] * 6
 
-cache_schedule = lambda org, month, year, schedule: None
-load_cached_schedule = lambda org, month, year: None
+cache_schedule = lambda dept, month, year, schedule: None
+load_cached_schedule = lambda dept, month, year: None
+
+
+#HELPERS
+def validate(builded_schedule: HorizonSchedule): #returns validation on the schedule
+    return builded_schedule, builded_schedule.feasible
 
 #start to generate a bunch of schedule data for a organization
 
+#RUNS ON-LOAD FOR HTTP ON MNGR SCREEN
 def build_full_month_schedule_with_pto(month_date: datetime.date, pto_map: Optional[Dict[datetime.date, Set[str]]]):
-    """Builds a monthly schedule with PTO (Paid Time Off) considerations. FULL month for entire org, all departments."""
-    depts = get_all_organization_departments()
+    """Builds a full month (all departments) schedule with PTO (Paid Time Off) considerations.
 
+    Args:
+        month_date (datetime.date): The date for the month to build the schedule for.
+        pto_map (Optional[Dict[datetime.date, Set[str]]]): A mapping of PTO dates to employee IDs.
+    """
+    depts = get_all_organization_departments()
+    all_dept_month_schedule = []
     for dept in depts:
-        people = get_people_from_organization(dept)
-        tasks = get_all_tasks_from_organization(dept, first, last)
-        build_month_schedule_with_pto_from_items(people, tasks, month_date, pto_map)
+        people = get_people_from_department(dept)
+        first = month_date.replace(day=1)
+        last = (first + datetime.timedelta(days=32)).replace(day=1) - datetime.timedelta(days=1) #next month first - 1 day = last of this month
+        tasks = get_all_tasks_from_department(dept, first, last)
+        all_dept_month_schedule.append(build_month_schedule_with_pto_from_items(people, tasks, month_date, pto_map))
+
+#DEPLOY TO MCP
+def test_employee_pto(department: object, employee: object, date: datetime.date):
+    """Tests the PTO (Paid Time Off) functionality for employees in a specific department.
+
+
+    Args:
+        department (object): The department to test the PTO of a specific employee.
+        employee (object): The employee to test the PTO for.
+        date (datetime.date): The date to test the PTO for.
+    """
+
+    schedule, validation = validate(build_month_schedule_with_pto_from_items(
+        get_people_from_department(department), 
+        get_all_tasks_from_department(department, date, date),
+        date,
+        {date: {employee.id,}} #TODO dict.update from cached pto data in THIS dept, in THIS timeframe
+    ))
+
+    if not validation:
+        #TODO mcp bad-ack 
+        pass
+    
+    else:
+        cache_schedule(department, date.month, date.year, schedule)
 
 def build_month_schedule_with_pto_from_items(people: List[object], tasks: List[object], month_date: datetime.date, pto_map: Optional[Dict[datetime.date, Set[str]]]):
-    """Builds a monthly schedule with PTO (Paid Time Off) considerations FROM people and task lists.
+    """Builds a monthly schedule (specific department) with PTO (Paid Time Off) considerations FROM people and task lists.
 
     Args:
         people (List[object]): _description_
@@ -38,29 +76,3 @@ def build_month_schedule_with_pto_from_items(people: List[object], tasks: List[o
     last = (first + datetime.timedelta(days=32)).replace(day=1) - datetime.timedelta(days=1)
     span = (last - first).days + 1
     return HorizonSchedulerPTO(people, tasks, first, span, int(datetime.datetime.now(tz=_TZ).timestamp()), allow_future=True, pto_map=pto_map).build() # type: ignore
-
-# def fill_month_schedule(today: datetime.date = datetime.date.today()):
-#     """TODO check cached dates against last 7 days to pre-populate preworked_in_last_7 for employees.
-
-#     Generates a schedule for all departments in an organization for the month specified at a generic date (in the month).
-#     Defaults to filling current month
-#     """
-#     first_of_month = today.replace(day=1)
-#     last_day = calendar.monthrange(today.year, today.month)[1]
-#     last_of_month = today.replace(day=last_day)
-
-#     month_span_days = (last_of_month - first_of_month).days + 1
-
-#     #goes dept for dept, gets all people and tasks, generates a schedule for the MONTH (monthview generator)
-#     for dept in get_all_organization_departments():  
-#         if not load_cached_schedule(dept, today.month, today.year):
-#             schedule = abstract_output(HorizonScheduler(
-#                 people=get_people_from_organization(dept), # type: ignore
-#                 tasks=get_all_tasks_from_organization(dept, first_of_month, last_of_month), # type: ignore
-#                 start_day=first_of_month,
-#                 span_days=month_span_days,
-#                 current_ts=today, # type: ignore
-#                 allow_future=True
-#             ))
-
-#             cache_schedule(dept, today.month, today.year, schedule)
